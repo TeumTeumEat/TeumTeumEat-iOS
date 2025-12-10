943259372248ce7f895d5878133dafd4a51c161b
LimJaeHyeon9298 <limjh9298@gmail.com>
2025-12-10 23:01:43 +0900

    Initial Commit

---
+++ TeumTeumEat/TeumTeumEat/TeumTeumEatApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeumTeumEatApp.swift
+//  TeumTeumEat
+//
+//  Created by 임재현 on 12/4/25.
+//
+
+import SwiftUI
+
+@main
+struct TeumTeumEatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
