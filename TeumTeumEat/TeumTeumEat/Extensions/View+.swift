db2dba19b57413b1d13caf95213e01834f2ca9ae
LimJaeHyeon9298 <limjh9298@gmail.com>
2025-12-11 17:00:42 +0900

    [feat]#1 Int,View extension 추가(숫자 포맷팅, 코너라운딩)

--- TeumTeumEat/TeumTeumEat/Extensions/View+.swift
+++ TeumTeumEat/TeumTeumEat/Extensions/View+.swift
@@ -12,3 +12,23 @@ extension View {
         UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
 }
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape(RoundedCorner(radius: radius, corners: corners))
+    }
+}
+
+struct RoundedCorner: Shape {
+    var radius: CGFloat
+    var corners: UIRectCorner
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(
+            roundedRect: rect,
+            byRoundingCorners: corners,
+            cornerRadii: CGSize(width: radius, height: radius)
+        )
+        return Path(path.cgPath)
+    }
+}
