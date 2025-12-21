1548d29d0c1885da44daffa1804fc3882dc0afdd
LimJaeHyeon9298 <limjh9298@gmail.com>
2025-12-21 23:12:24 +0900

    [feat] #6 OnboardingData 모델링 작업

---
+++ TeumTeumEat/TeumTeumEat/Features/Onboarding/Models/OnboardingData.swift
@@ -0,0 +1,15 @@
+//
+//  OnboardingData.swift
+//  TeumTeumEat
+//
+//  Created by 임재현 on 12/21/25.
+//
+
+import Foundation
+
+struct OnboardingData: Equatable, Codable {
+    var userName: String = ""
+    var leaveHomeTime: Date?
+    var returnHomeTime: Date?
+    var dailyUsageMinutes: Int = 0
+}
