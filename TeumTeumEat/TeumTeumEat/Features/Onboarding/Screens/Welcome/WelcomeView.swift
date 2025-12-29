3a3bd4cb3b3c6d25a6e78f7c8d87d1721d493b62
LimJaeHyeon9298 <limjh9298@gmail.com>
2025-12-29 12:51:29 +0900

    [fix] #10 WelcomeView 바뀐 디자인 적용

--- TeumTeumEat/TeumTeumEat/Features/Onboarding/Screens/Welcome/WelcomeView.swift
+++ TeumTeumEat/TeumTeumEat/Features/Onboarding/Screens/Welcome/WelcomeView.swift
@@ -13,16 +13,30 @@ struct WelcomeView: View {
     
     var body: some View {
         VStack(spacing: 0) {
-            Spacer()
+            VStack(spacing: 0) {
+                Text("매일 소모되는 이동 시간,")
+                    .titleSemibold18()
+                
+                Text("틈틈잇과 함께 성장하는 시간으로 바꿔봐요!")
+                    .titleSemibold18()
+            }
+            .multilineTextAlignment(.center)
+            .padding(.top, 70)
             
-            Text("서비스 소개애애애애")
-                .titleSemibold18()
-                .padding(.bottom, 107)
+            
+            Image("logo_login")
+                .resizable()
+                .renderingMode(.template)
+                .foregroundStyle(.black)
+                .frame(width: 177,height: 54)
+                .padding(.top, 21)
+
             
             Image("pose=front")
                 .frame(maxWidth: .infinity)
                 .frame(height: 244)
                 .padding(.horizontal, 30)
+                .padding(.top, 25)
             
             Spacer()
                 
