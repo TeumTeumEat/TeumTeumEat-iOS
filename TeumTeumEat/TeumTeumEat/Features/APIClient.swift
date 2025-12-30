a3c42176022a71de51a7f474cd194b0a62f9b849
LimJaeHyeon9298 <limjh9298@gmail.com>
2025-12-30 19:50:49 +0900

    [feat] #15 HTTP Method enum 타입 정의

---
+++ TeumTeumEat/TeumTeumEat/Features/APIClient.swift
@@ -0,0 +1,16 @@
+//
+//  APIClient.swift
+//  TeumTeumEat
+//
+//  Created by 임재현 on 12/30/25.
+//
+
+import Foundation
+import Dependencies
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+}
